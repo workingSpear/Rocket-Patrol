f38bea7b7fd30f8c4654b93c19f70a92ba4c7170
Kait Srivastav <asriva28@ucsc.edu>
2026-01-19 19:47:00 -0800

    finish game

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,5 +1,3 @@
-const { Phaser } = require("../../lib/phaser");
-
 // Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y, texture, frame) {
@@ -7,36 +5,44 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
     scene.add.existing(this); // adds to existing, displayList, updateList
     this.isFiring = false;    // tracks the rocket's state
-    this.moveSpeed = 2;       // rocket's speed in pixels/frame
+    this.moveSpeed = 0.2;       // rocket's speed in pixels/frame
+    this.sfxShot = scene.sound.add('sfx-shot');
   }
 
-  update() {
+  update(delta) {
+    let speedThisFrame = this.moveSpeed * delta;
+
     // left/right movement
     if(!this.isFiring) {
       if (keyLEFT.isDown && this.x >= borderUISize + this.width) {
-        this.x -= this.moveSpeed;
+        this.x -= speedThisFrame;
       }
       else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width)
       {
-        this.x += this.moveSpeed;
+        this.x += speedThisFrame;
+      }
       }
 
       //fire
-      if(Phaser.Input.Keyboard.JustDown(keyFIRE)) {
+      if(Phaser.Input.Keyboard.JustDown(keyFIRE) && !this.isFiring) {
         this.isFiring = true;
+        this.sfxShot.play();
       }
 
       // move up based on firing state
-      if(this.isFiring && this.y >= borderPaddingUISize * 3 + borderPadding) {
-        this.y -= this.moveSpeed;
+      if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+        this.y -= speedThisFrame;
       }
 
       // reset on miss
       if(this.y <= borderUISize * 3 + borderPadding) {
-        this.isFiring = false;
-        this.y = game.config.height - borderUISize - borderPadding;
+        this.reset();
       }
-      
-    }
+  }
+
+  // reset rocket to "ground"
+  reset() {
+    this.isFiring = false;
+    this.y = game.config.height - borderUISize - borderPadding;
   }
 }
